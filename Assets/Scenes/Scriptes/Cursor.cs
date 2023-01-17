caafe2cbb56201c6d37dfef5a430e14f16163b94
sasanonoha <k020g1406@g.neec.ac.jp>
2023-01-17 18:09:55 +0900

    ・敵撃破の処理がなぜか消えてしまっていたので再度作り直した。（CrushingManager）

--- Assets/Scenes/Scriptes/Cursor.cs
+++ Assets/Scenes/Scriptes/Cursor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Cursor : MonoBehaviour
 {
     // 座標用の変数
-    private Vector2 mousePos, target;
+    private Vector3 mousePos, target;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +16,16 @@ public class Cursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // マウス座標の取得
+        //マウス座標の取得
         mousePos = Input.mousePosition;
-        // マウスの座標を代入する
+
+        // ワールド座標の照準になる。
         transform.position = mousePos;
+    }
+
+    private void CreateShot(float axis)
+    {
+        // ベクトルを取得
+        //var dirction = transform.position - P
     }
 }
