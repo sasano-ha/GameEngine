f8d216387eaba0c5a0cb892be2f4f65486aee4e3
sasanonoha <k020g1406@g.neec.ac.jp>
2023-02-15 16:13:44 +0900

    ・スコアのスクリプトを作った。 ・Hpの文字が真ん中に来て違和感がなくなった。

--- Assets/Scenes/Scriptes/Player/GameObjectMove.cs
+++ Assets/Scenes/Scriptes/Player/GameObjectMove.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class GameObjectMove : MonoBehaviour
 {
+    public static GameObjectMove instance;
+
+    public Vector3 gameobject;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            // インスタンスの設定
+            instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
