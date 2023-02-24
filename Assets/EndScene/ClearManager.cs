745916fdf9d5a4de410b7a76a2c2ce00c67d1128
sasanonoha <k020g1406@g.neec.ac.jp>
2023-02-24 16:14:19 +0900

    ・ゲームシーンの時にマウスカーソルを描画させていなかったためクリアシーンの時にどこにカーソルがあるのかわからなくボタンを押せなかったのでクリアシーンはカーソルは描画させるようにした。

--- Assets/EndScene/ClearManager.cs
+++ Assets/EndScene/ClearManager.cs
@@ -8,7 +8,7 @@ public class ClearManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
