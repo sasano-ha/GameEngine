845fd5008eefdfd28f94a2cfd824bb3457b0d59e
sasanonoha <k020g1406@g.neec.ac.jp>
2023-03-03 11:23:39 +0900

    ・一部スクリプトのリファクタリング（書いてある処理のコメントアウトの追加）

--- Assets/Scenes/Scriptes/Enemy/Enemy.cs
+++ Assets/Scenes/Scriptes/Enemy/Enemy.cs
@@ -7,15 +7,19 @@ public class Enemy: MonoBehaviour
     // Enemyの体力用変数
     private int enemyHp;
 
+    // 狙いたい相手の変数。
     private Vector3 targetpos;
 
     // gameManager呼び出し
     public GameManager gameManager;
 
+    // スコアクラスの呼び出し。
     public Score scores;
 
+    // アタッチするためのもの
     [SerializeField] GameObject explosionPrefab;
 
+    // カメラのフラグ
     private bool isInsideCamera = true;
 
     // Start is called before the first frame update
@@ -24,56 +28,76 @@ public class Enemy: MonoBehaviour
         // 生成時に体力を指定しておく
         enemyHp = 2;
 
+        // 変数に格納する。
         targetpos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 敵がカメラ内にいる時
         if(isInsideCamera == true)
         {
-            targetpos.z -= 0.005f;
+            // スピードを与える
+            //targetpos.z -= 0.005f;
 
-            if (targetpos.z <= 2)
-            {
-                targetpos.x += 0.1f;
-                targetpos.z -= 0.1f;
-            }
+            //if (targetpos.z <= 2)
+            //{
+            //    targetpos.x += 0.1f;
+            //    targetpos.z -= 0.1f;
+            //}
 
             //targetpos.x -= 0.01f;
 
+
+            // 位置の更新
             transform.position = new Vector3(Mathf.Sin(Time.time) * 2.0f + targetpos.x, targetpos.y, targetpos.z);
 
             // もし体力が0以下になったら
             if (enemyHp <= 0 )
             {
+                // enemyダウン数関数の呼び出し。
                 gameManager.AddCrushingCount();
+
+                // enemyダウンスコア関数の呼び出し。
                 scores.AddDownScore();
+
+                // 爆発パーティクル
                 Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-                // 自分で消える
+
+                // 自分で消える。
                 Destroy(this.gameObject);
             }
         }
-        else if(isInsideCamera == false)
+
+        // enemyがカメラ外に行った時
+        else if (isInsideCamera == false)
         {
+            // enemyのオブジェクトを消す。
             Destroy(this.gameObject);
         }
         
     }
 
+    // 当たり判定
     private void OnTriggerEnter(Collider other)
     {
+        // "Player"のタグを探す。
         if (other.gameObject.tag == "Player")
         {
+            // playerダメージ関数の呼び出し。
             other.GetComponent<PlayerHp>().Damage();
         }
     }
 
+
+    // カメラフラグ関数
     private void OnBecameInvisible()
     {
         isInsideCamera = false;
     }
 
+    // enemyダメージ関数
     public void Damage()
     {
         enemyHp--;
