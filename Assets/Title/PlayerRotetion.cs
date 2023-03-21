2fc30fcc210dfd1d3b5a58edb948d4beb73f6706
sasanonoha <k020g1406@g.neec.ac.jp>
2023-03-21 23:12:55 +0900

    ・弾をオブジェクトからパーティクルにしたが、思ったより見えずらいのでテクスチャを入れてどうにかしたいと思う。

--- Assets/Title/PlayerRotetion.cs
+++ Assets/Title/PlayerRotetion.cs
@@ -7,6 +7,8 @@ public class PlayerRotetion : MonoBehaviour
 
     //private Quaternion player;
 
+    private Vector3 titlePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,12 @@ public class PlayerRotetion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //titlePlayer += new Vector3(titlePlayer.x + 10, titlePlayer.y, titlePlayer.z);
+
+        //transform.position += new Vector3(10, 0, 0);
+
+        //transform.position += new Vector3(0, 0, 1) * Time.deltaTime;s
+
         gameObject.transform.Rotate(new Vector3(0.0f, 10.0f, 0.0f) * Time.deltaTime);
     }
 }
